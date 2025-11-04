1b6b3fce26cde07e2aaf3be69aefb6d427520295
paulotks <pauloef.lopes@gmail.com>
2025-11-04 15:46:53 -0300

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ConversartionRelayBR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
