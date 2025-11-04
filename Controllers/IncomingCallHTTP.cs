66c91d4dd08d3c435e7ceee00cc0ba65b6065828
paulotks <pauloef.lopes@gmail.com>
2025-11-04 16:08:50 -0300

    start minimal websocket

---
+++ Controllers/IncomingCallHTTP.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Twilio.TwiML;
+using Twilio.TwiML.Voice;
+
+namespace ConversartionRelayBR.Controllers
+{
+    [Route("/")]
+    public class IncomingCallHTTP : Controller
+    {
+        [HttpPost]
+        public IActionResult IncomingCallTwilioHTTP()
+        {
+            var response = new VoiceResponse();
+            var connect = new Connect();
+
+            var conversationRelay = new ConversationRelay(
+                url: "wss://telma-unswitched-walton.ngrok-free.dev/websocket",
+                welcomeGreeting: "Bem vindo ao canal de atendimento da FGR, Este é um canal em desenvolvimento. Obrigado por ligar",
+                language: "pt-BR"
+                );
+
+            //conversationRelay.Hints = "FGR,atendimento,conta corrente,poupança,cartão,empréstimo,financiamento,PIX,transferência,saldo,extrato,segunda via,cancelamento,bloqueio,desbloqueio,por favor,obrigada,com licença,desculpa,problema,dúvida,informação";
+
+
+            connect.Append(conversationRelay);
+            response.Append(connect);
+
+            Console.WriteLine(response.ToString());
+
+            return Content(response.ToString(), "application/xml");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,22 @@
+using Twilio;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllers();
 
 var app = builder.Build();
+
+var accountSid = app.Configuration["TWILIO_ACCOUNT_SID"];
+var authToken = app.Configuration["TWILIO_AUTH_TOKEN"];
+
+// Validate configuration
+if (string.IsNullOrEmpty(accountSid) || string.IsNullOrEmpty(authToken))
+{
+    throw new InvalidOperationException("Twilio credentials not found in configuration. Please check your appsettings.json file.");
+}
+
+TwilioClient.Init(accountSid, authToken);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -13,17 +26,63 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+app.UseWebSockets();
+
+//app.UseHttpsRedirection();
 app.UseRouting();
 
 app.UseAuthorization();
 
 app.MapStaticAssets();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+app.MapControllers();
 
+app.Map("/websocket", async (HttpContext context) =>
+{
+    if (context.WebSockets.IsWebSocketRequest)
+    {
+        Console.WriteLine("Chegou no WebSocket");
+        using var websocket = await context.WebSockets.AcceptWebSocketAsync();
+        await HandleWebSocketConnection(websocket);
+    }
+    else
+    {
+        context.Response.StatusCode = 400;
+    }
+});
 
 app.Run();
+
+static async Task HandleWebSocketConnection(System.Net.WebSockets.WebSocket webSocket)
+{
+    var buffer = new byte[1024 * 4];
+
+    while (webSocket.State == System.Net.WebSockets.WebSocketState.Open)
+    {
+        var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+        if (result.MessageType == System.Net.WebSockets.WebSocketMessageType.Text)
+        {
+            var message = System.Text.Encoding.UTF8.GetString(buffer, 0, result.Count);
+            Console.WriteLine($"RecebidoTwilio: {message}");
+
+            //var response = "Echo: " + message;
+            //var responseBytes = System.Text.Encoding.UTF8.GetBytes(response);
+
+            //await webSocket.SendAsync(
+            //    new ArraySegment<byte>(responseBytes),
+            //    System.Net.WebSockets.WebSocketMessageType.Text,
+            //    true,
+            //    CancellationToken.None
+            //    );
+        }
+        else if (result.MessageType == System.Net.WebSockets.WebSocketMessageType.Close)
+        {
+            await webSocket.CloseAsync(
+                System.Net.WebSockets.WebSocketCloseStatus.NormalClosure,
+                "",
+                CancellationToken.None
+            );
+        }
+    }
+}
